9e2116ea54ecd1cee393cefbc511f3bc084f7240
Joel Cooper <jodycooper16@gmail.com>
2021-04-12 15:46:43 -0700

    commit tree f3de14050bf7e444b69b228d7df4f09b5564cdb3

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (strings) => {
+  return tutorials.map(val => {
+    const wArr = val.split(" ")
+    const tutorial =
+    wArr.map( wArr => wArr.charAt(0).toUpperCase() + wArr.slice(1) )
+    const finArr = tutorial.join(' ')
+
+  //   return val.replace(val.charAt(0), val.charAt(0).toUpperCase())
+  // })
+  return finArr
+  })
 }
+
+  
+//   let wArr= stLowerCase.split(" ").map(function(word) {
+//       return currentValue[0].toUpperCase() + word.substring(1);
+//   });
+
+//   return wArr.join(" ");
+// }
+
+
+
+// let titleCased = (tutorial1)
+// {tutorials.map(val => 
+//   val.slice(1).toUpperCase() ===val.slice(1) ? val[0].toUpperCase()+ val.slice(1).toLowerCase() : val[0].toUpperCase() + val.slice(1));
+// return titleCased}
+
+
+// function titleCase(strings){ 
+//   let strings = tutorials.map(function(val){
+//     tutorials.split(' ')
+//     return val.replace(val.charAt(0), val.charAt(0).toUpperCase())
+    
+     
+
+//   })
+  
+
+
+
